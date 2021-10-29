6983ffd2df2305ce590446954653a063704d4682
Jan Janech <jan.janech@kst.uniza.sk>
2021-10-29 11:42:05 +0200

    05b - odrazenie lopty od okraja obrazovky

--- pong/pong/Lopta.java
+++ pong/pong/Lopta.java
@@ -1,5 +1,8 @@
 public class Lopta {
     private Kruh lopta;
+    private int smerX;
+    private int smerY;
+    private int poziciaY;
     
     public Lopta() {
         this.lopta = new Kruh();
@@ -7,12 +10,21 @@ public class Lopta {
         this.lopta.posunVodorovne(125);
         this.lopta.posunZvisle(85);
         this.lopta.zobraz();
+        
+        this.poziciaY = 150;
+        
+        this.smerX = 5;
+        this.smerY = 5;
     }
     
-    public void posun() {
+    public void tik() {
+        if (this.poziciaY == 5 || this.poziciaY == 295) {
+            this.smerY = -this.smerY;
+        }
         this.lopta.skry();
-        this.lopta.posunVodorovne(5);
-        this.lopta.posunZvisle(5);
+        this.lopta.posunVodorovne(this.smerX);
+        this.lopta.posunZvisle(this.smerY);
         this.lopta.zobraz();
+        this.poziciaY += this.smerY;
     }
 }
