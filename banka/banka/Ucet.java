648e396d47364a85a60ad7e1f619d3306d891852
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-11 09:21:19 +0100

    07a - vyber a vklad

--- banka/banka/Ucet.java
+++ banka/banka/Ucet.java
@@ -8,4 +8,17 @@ public class Ucet {
         this.menoVlastnika = menoVlastnika;
         this.cisloUctu = cisloUctu;
     }
+    
+    public double getStav() {
+        return this.stavUctuVCentoch / 100.0;
+    }
+    
+    public void vloz(double suma) {
+        // this.stavUctuVCentoch = this.stavUctuVCentoch + suma * 100;
+        this.stavUctuVCentoch += suma * 100;
+    }
+    
+    public void vyber(double suma) {
+        this.stavUctuVCentoch -= suma * 100;
+    }
 }
