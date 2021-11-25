69cef7ff33913509dce2fdc326fc509257db6efe
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-25 09:29:28 +0100

    09a - nakreslenie celej sachovnice

--- nim/Sachovnica.java
+++ nim/Sachovnica.java
@@ -1,13 +1,15 @@
 public class Sachovnica {
     public Sachovnica(int sirka, int vyska) {
-        for (int x = 0; x < sirka; x++) {
-            Stvorec policko = new Stvorec();
-            policko.zmenStranu(20);
-            policko.posunZvisle(-50);
-            policko.posunVodorovne(-60 + x*20);
-            policko.zmenFarbu("black");
-            if (x % 2 == 0) {
-                policko.zobraz();
+        for (int y = 0; y < vyska; y++) {
+            for (int x = 0; x < sirka; x++) {
+                Stvorec policko = new Stvorec();
+                policko.zmenStranu(20);
+                policko.posunZvisle(-50 + y*20);
+                policko.posunVodorovne(-60 + x*20);
+                policko.zmenFarbu("black");
+                if (x % 2 == y % 2) {
+                    policko.zobraz();
+                }
             }
         }
     }
