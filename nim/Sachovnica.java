b63d0da9e45b54bd22dc24bde778351ec2113041
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-26 11:52:34 +0100

    09b - zobrazovanie/skryvanie sachovnice

--- nim/Sachovnica.java
+++ nim/Sachovnica.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Sachovnica {
+    private ArrayList<Stvorec> policka;
+    
     public Sachovnica(int sirka, int vyska) {
+        this.policka = new ArrayList<Stvorec>();
+        
         for (int y = 0; y < vyska; y++) {
             for (int x = 0; x < sirka; x++) {
                 Stvorec policko = new Stvorec();
@@ -8,9 +14,21 @@ public class Sachovnica {
                 policko.posunVodorovne(-60 + x*20);
                 policko.zmenFarbu("black");
                 if (x % 2 != (vyska - y) % 2) {
-                    policko.zobraz();
+                    this.policka.add(policko);
                 }
             }
+        }
+    }
+    
+    public void zobraz() {
+        for (Stvorec policko : this.policka) {
+            policko.zobraz();
+        }
+    }
+    
+    public void skry() {
+        for (Stvorec policko : this.policka) {
+            policko.skry();
         }
     }
 }
