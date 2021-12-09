d2b2e31c3340e182957da33107b7fe30bb57d889
Jan Janech <jan.janech@kst.uniza.sk>
2021-12-09 09:42:32 +0100

    11a - moznost zasvietit/zhasnut pixel na displeji

--- tetris/tetris/Displej.java
+++ tetris/tetris/Displej.java
@@ -3,7 +3,11 @@ public class Displej {
     private static final int SIRKA = 10;
     private static final int VELKOST_PIXELU = 15;
     
+    private Stvorec[][] pixely;
+    
     public Displej() {
+        this.pixely = new Stvorec[Displej.VYSKA][Displej.SIRKA];
+        
         for (int y = 0; y < Displej.VYSKA; y++) {
             for (int x = 0; x < Displej.SIRKA; x++) {
                 Stvorec pixel = new Stvorec();
@@ -11,11 +15,17 @@ public class Displej {
                 pixel.posunVodorovne(-60 + x * Displej.VELKOST_PIXELU);
                 pixel.posunZvisle(-50 + y * Displej.VELKOST_PIXELU);
                 pixel.zobraz();
+                
+                this.pixely[y][x] = pixel;
             }
         }
     }
     
     public void nastav(int x, int y, boolean svieti) {
-        
+        if (svieti) {
+            this.pixely[y][x].zmenFarbu("yellow");
+        } else {
+            this.pixely[y][x].zmenFarbu("red");
+        }
     }
 }
