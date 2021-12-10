9f7b72adacfcf02b313c0cd1e4e9e8b134d0f0d6
Jan Janech <jan.janech@kst.uniza.sk>
2021-12-10 12:13:14 +0100

    11b - tetromino sa zobrazuje hore v strede displeja

--- tetris/tetris/Tetromino.java
+++ tetris/tetris/Tetromino.java
@@ -1,8 +1,11 @@
 public class Tetromino {
     public Tetromino(boolean[][] tvar) {
+        int poziciaX = (Displej.SIRKA - tvar[0].length) / 2;
+        int poziciaY = 0;
+        
         for (int y = 0; y < tvar.length; y++) {
             for (int x = 0; x < tvar[0].length; x++) {
-                Displej.getDisplej().nastav(x, y, tvar[y][x]);
+                Displej.getDisplej().nastav(x + poziciaX, y + poziciaY, tvar[y][x]);
             }
         }
     }
